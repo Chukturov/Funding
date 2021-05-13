a9e33f31c40c9600a9b664b8d7c891fdbce84c0e
Chukturov <chukturov98@gmail.com>
2021-05-13 10:31:51 +0300

    Корректное отображение лайков

---
+++ Funding/Migrations/20210513072053_deletePostsWithLikes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Funding.Migrations
+{
+    public partial class deletePostsWithLikes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
